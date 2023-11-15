f23849a451b9c5a6aeffb0130e00fb5aa849f592
Leonardo Piovesan <leopvsn@gmail.com>
2023-11-15 14:21:27 -0300

    feat: layout and page setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header: React.FC = () => (
+  <header className="bg-blue-500 p-4 text-white">
+    <h1 className="text-xl">My Portfolio</h1>
+  </header>
+);
+
+export default Header;
