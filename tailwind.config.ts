5b1f18ef78f8f4afc1dc3c6c4d695825c3578691
Leonardo Piovesan <leopvsn@gmail.com>
2023-11-18 11:21:51 -0300

    chore: add TODO.md to gitignore, somes configs to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "5xl": "2.5rem",
+      },
+    },
   },
   plugins: [],
 };
