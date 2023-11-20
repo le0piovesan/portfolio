9c08e4c346122c257031233ac8b725384624cae6
Leonardo Piovesan <leopvsn@gmail.com>
2023-11-20 11:26:58 -0300

    feat: about/main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Header from "@/components/common/Header";
+import Header from "@/components/sections/Header";
+import Main from "@/components/sections/Main";
 
 const Home: React.FC = () => (
   <div>
     <Header />
-    <main></main>
+    <Main />
   </div>
 );
 
---
+++ src/components/sections/Main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import About from "../layout/Main/About";
+
+const Main: React.FC = () => (
+  <main>
+    <About />
+  </main>
+);
+
+export default Main;
