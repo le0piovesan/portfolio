4e642d3a86288d5c82c3a797dac1d1b8d4d2babc
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-03 12:42:11 -0300

    fix: avatar image set priority for LCP warning

--- src/components/layout/Header/Avatar.tsx
+++ src/components/layout/Header/Avatar.tsx
@@ -6,6 +6,7 @@ const Avatar: React.FC = () => (
   <div className="relative pr-0 md:pr-8">
     <div className="absolute inset-0 z-0 h-full w-full translate-x-[-30%] -rotate-45 transform bg-orange-400 opacity-40"></div>
     <Image
+      priority
       src={Me}
       className="relative z-10 mx-auto mb-4 h-[267px] w-[220px]"
       alt="Hello, it's a picture of me"
