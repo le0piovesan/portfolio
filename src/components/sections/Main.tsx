d26c36fead8bd463ef9fdf669933e8280e569e75
Leonardo Piovesan <leopvsn@gmail.com>
2023-12-03 12:47:23 -0300

    feat: importing Projects component into Main section

--- src/components/sections/Main.tsx
+++ src/components/sections/Main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import About from "../layout/Main/About";
+import Projects from "../layout/Main/Projects";
 
 const Main: React.FC = () => (
   <main>
     <About />
+    <Projects />
   </main>
 );
 
